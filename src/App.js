91b40c08e0b172e9ea483bee5c65b0c145a5e628
islah <islah.musleh@wallet-codes.com>
2023-02-27 12:12:53 +0800

    make a test change to see if it reflect on AWS amplify

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React less than a month
+          Learn React less than a day
         </a>
       </header>
     </div>
